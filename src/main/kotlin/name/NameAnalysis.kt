0c8b622fcd364309823d36beb939ede4f126a488
Nathan Coquerel <nathan.coquerel.ext@univ-lille.fr>
2026-03-11 12:30:12 +0100

    :sparkles: Add RewriteRuleNameDefinitionAndUsage to check if used names are rightfully defined (and not multiple times) in the rule pattern

--- src/main/kotlin/name/NameAnalysis.kt
+++ src/main/kotlin/name/NameAnalysis.kt
@@ -2,6 +2,49 @@ package fr.univ_lille.iut_info.name
 
 import fr.univ_lille.iut_info.*
 
+
+fun Pattern.variables(): List<String> {
+    if (this is StringPattern) return emptyList()
+    if (this is NumberPattern) return emptyList()
+    if (this is ChildrenPattern) return this.patterns.flatMap { it.variables() }
+    if (this is ObjectPattern) return listOf(
+        if (this.alias != null) listOf(alias) else emptyList(),
+        fields.flatMap { it.second.variables() },
+        if (this.children != null) children.variables() else emptyList()
+    ).flatten()
+    if (this is ListPattern) return this.patterns.flatMap { it.variables() }
+    throw IllegalStateException("Unknown pattern type")
+}
+
+fun Transform.variables(): List<String> {
+    if (this is DeleteTransform) return emptyList()
+    if (this is ExpressionTransform) return expression.variables()
+    if (this is ObjectTransform) {
+        return listOf(
+            fields.flatMap { it.second.variables() },
+            if (this.children != null) children.variables() else emptyList()
+        ).flatten()
+    }
+    if (this is ChildrenTransform) return this.transforms.flatMap { it.variables() }
+    if (this is ListTransform) return this.transforms.flatMap { it.variables() }
+    throw IllegalStateException("Unknown transform type")
+}
+
+fun Expression.variables(): List<String> {
+    if (this is ValueExpression) return emptyList()
+    if (this is IdentifierExpression) return listOf(identifiers[0])
+    throw IllegalStateException("Unknown expression type")
+}
+
+fun Condition.variables(): List<String> {
+    if (this is TrueCondition) return emptyList()
+    if (this is AndCondition) return left.variables() + right.variables()
+    if (this is OrCondition) return left.variables() + right.variables()
+    if (this is EqualCondition) return left.variables() + right.variables()
+    if (this is NotCondition) return operand.variables()
+    throw IllegalStateException("Unknown condition type")
+}
+
 class NameAnalysis(val program: List<Statement>) {
     val names: MutableMap<String, Identified> = HashMap()
 
@@ -16,7 +59,8 @@ class NameAnalysis(val program: List<Statement>) {
                 .flatMap(this::checkObjectPattern).toList(),
             program.filterIsInstance<RewriteRuleStatement>().map { (_, _, transform) -> transform }
                 .filterIsInstance<ObjectTransform>()
-                .flatMap(this::checkTransform)
+                .flatMap(this::checkTransform),
+            program.filterIsInstance<RewriteRuleStatement>().flatMap(this::checkRewriteRuleNameDefinitionAndUsage)
         ).flatten().toSet().toList()
     }
 
@@ -32,7 +76,7 @@ class NameAnalysis(val program: List<Statement>) {
             type.children.associateBy({ it.first }, { (key, _) -> type.children.count({ it.first == key }) })
                 .filterValues { it > 1 }.keys
 
-        return duplicateKeys.map { "NameError: $name contains a duplicate field $it" }
+        return duplicateKeys.map { "NameError: $name contains a duplicate field $it." }
     }
 
     fun checkObjectPattern(type: ObjectPattern): List<String> {
@@ -40,7 +84,7 @@ class NameAnalysis(val program: List<Statement>) {
             type.fields.associateBy({ it.first }, { (key, _) -> type.fields.count({ it.first == key }) })
                 .filterValues { it > 1 }.keys
 
-        return duplicateKeys.map { "NameError: A patterns contains a duplicate field $it" }
+        return duplicateKeys.map { "NameError: A patterns contains a duplicate field $it." }
     }
 
     fun checkTransform(type: ObjectTransform): List<String> {
@@ -48,7 +92,24 @@ class NameAnalysis(val program: List<Statement>) {
             type.fields.associateBy({ it.first }, { (key, _) -> type.fields.count({ it.first == key }) })
                 .filterValues { it > 1 }.keys
 
-        return duplicateKeys.map { "NameError: A pattern transformation contains a duplicate field $it" }
+        return duplicateKeys.map { "NameError: A pattern transformation contains a duplicate field $it." }
 
+    }
+
+    fun checkRewriteRuleNameDefinitionAndUsage(rule: RewriteRuleStatement): List<String> {
+        val existing = rule.pattern.variables()
+        val usedByCondition = rule.condition.variables()
+        val usedByTransform = rule.transform.variables()
+
+        val duplicateKeys =
+            existing.associateBy({ it }, { key -> existing.count({ it == key }) })
+                .filterValues { it > 1 }.keys
+
+        return listOf(
+            duplicateKeys.map { "NameError: The variable $it is defined multiple times in a pattern." },
+            usedByCondition.filter { !(existing.contains(it)) }
+                .map { "NameError: Variable $it is used in a condition but is not defined." },
+            usedByTransform.filter { !(existing.contains(it)) }
+                .map { "NameError: Variable $it is used in a transformation but is not defined." }).flatten()
     }
 }
