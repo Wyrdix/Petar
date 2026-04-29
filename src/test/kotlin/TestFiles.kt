2fd70fd5b3aff1cf48062c9244d341acf046775b
Nathan Coquerel <nathan.coquerel.ext@univ-lille.fr>
2026-04-29 11:41:05 +0200

    :sparkles: Have the test files support multiple input/output per specification environments

--- src/test/kotlin/TestFiles.kt
+++ src/test/kotlin/TestFiles.kt
@@ -10,39 +10,51 @@ import kotlin.test.assertEquals
 class TestFiles {
     @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     @TestFactory
-    fun dynamicTestsFromFiles(): Stream<DynamicTest> {
+    fun testsFromFiles(): Stream<DynamicTest> {
         val file = File("src/test/test_files")
-        println("${file.exists()} ${file.path}")
         return file.walk().filter { parent -> parent.isDirectory() && parent.listFiles().none { it.isDirectory } }
-            .map { directory ->
+            .flatMap { directory ->
 
                 val specifications = directory.listFiles().filter { it.extension == "alfr" }
-                val input = File(directory, "input.json")
-                val expected = File(directory, "output.json")
-                val actual = File(directory, "output.json.actual")
 
-                if (actual.exists()) actual.delete()
+                val subTests = directory.listFiles()
+                    .filter { it.nameWithoutExtension.startsWith("input") && it.extension == "json" }
 
-                val testName = directory.toRelativeString(file)
-                return@map DynamicTest.dynamicTest(
-                    testName
-                ) {
-                    try {
-                        main(
-                            specifications.flatMap { listOf("-s", it.absolutePath) }.toTypedArray() + arrayOf(
-                                "-r",
-                                "Root"
-                            ) + arrayOf("-i", input.absolutePath) + arrayOf("-o", actual.absolutePath)
-                        )
+                subTests
+                    .map { input ->
+                        val radical = input.nameWithoutExtension.substring("input".length)
+                        val expected =
+                            File(directory, "output$radical.json")
 
-                        assertEquals(Files.readString(expected.toPath()), Files.readString(actual.toPath()))
-                    } catch (e: Exception) {
-                        throw e
-                    } finally {
-                        if (actual.exists()) actual.delete()
+                        val actual = File(expected.absolutePath + ".actual")
+                        val testName =
+                            if (subTests.size == 1) directory.toRelativeString(file)
+                            else directory.toRelativeString(file) + "/${radical}"
+
+                        DynamicTest.dynamicTest(
+                            testName
+                        ) {
+                            try {
+                                main(
+                                    specifications.flatMap { listOf("-s", it.absolutePath) }
+                                        .toTypedArray() + arrayOf(
+                                        "-r", "Root"
+                                    ) + arrayOf("-i", input.absolutePath) + arrayOf("-o", actual.absolutePath)
+                                )
+
+                                assertEquals(
+                                    Files.readString(expected.toPath()),
+                                    Files.readString(actual.toPath())
+                                )
+                            } catch (e: Exception) {
+                                throw e
+                            } finally {
+                                if (actual.exists()) actual.delete()
+                            }
+
+                        }
                     }
 
-                }
             }.asStream()
     }
 }
